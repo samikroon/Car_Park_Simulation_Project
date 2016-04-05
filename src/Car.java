83efcf7069d24ff6617b75c456fbd774847d4a56
samikroon <samikroon@gmail.com>
2016-04-05 12:11:03 +0200

    Test commit

---
+++ src/Car.java
@@ -0,0 +1,44 @@
+public abstract class Car {
+
+    private
+    Location location;
+    private int minutesLeft;
+    private boolean isPaying;
+
+    /**
+     * Constructor for objects of class Car
+     */
+    public Car() {
+
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public int getMinutesLeft() {
+        return minutesLeft;
+    }
+
+    public void setMinutesLeft(int minutesLeft) {
+        this.minutesLeft = minutesLeft;
+    }
+
+    public boolean getIsPaying() {
+        return isPaying;
+    }
+
+    public void setIsPaying(boolean isPaying) {
+        this.isPaying = isPaying;
+    }
+
+    public void tick() {
+        System.out.println("car-tick");
+        minutesLeft--;
+    }
+
+}
---
+++ src/Simulator.java
@@ -0,0 +1,181 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
+public class Simulator implements ActionListener{
+
+    private CarQueue entranceCarQueue;
+    private CarQueue paymentCarQueue;
+    private CarQueue exitCarQueue;
+    private SimulatorView simulatorView;
+    private ActionEvent event;
+
+    private int day = 0;
+    private int hour = 0;
+    private int minute = 0;
+
+    private int tickPause = 100;
+
+    int weekDayArrivals= 50; // average number of arriving cars per hour
+    int weekendArrivals = 90; // average number of arriving cars per hour
+
+    int enterSpeed = 3; // number of cars that can enter per minute
+    int paymentSpeed = 10; // number of cars that can pay per minute
+    int exitSpeed = 9; // number of cars that can leave per minute
+
+    public Simulator() {
+        entranceCarQueue = new CarQueue();
+        paymentCarQueue = new CarQueue();
+        exitCarQueue = new CarQueue();
+        simulatorView = new SimulatorView(3, 6, 30, this);
+    }
+
+    public void run() {
+        for (int i = 0; i < 10000; i++) {
+            tick();
+        }
+    }
+
+    public void runSteps(int steps) {
+        System.out.println("run " + steps + " steps");
+        for (int i = 0; i < steps; i++) {
+            tick();
+        }
+    }
+
+    private void tick() {
+        System.out.println("simulator-tick");
+        // Advance the time by one minute.
+        minute++;
+        while (minute > 59) {
+            minute -= 60;
+            hour++;
+        }
+        while (hour > 23) {
+            hour -= 24;
+            day++;
+        }
+        while (day > 6) {
+            day -= 7;
+        }
+
+        Random random = new Random();
+
+        // Get the average number of cars that arrive per hour.
+        int averageNumberOfCarsPerHour = day < 5
+                ? weekDayArrivals
+                : weekendArrivals;
+
+        // Calculate the number of cars that arrive this minute.
+        double standardDeviation = averageNumberOfCarsPerHour * 0.1;
+        double numberOfCarsPerHour = averageNumberOfCarsPerHour + random.nextGaussian() * standardDeviation;
+        int numberOfCarsPerMinute = (int)Math.round(numberOfCarsPerHour / 60);
+
+        // Add the cars to the back of the queue.
+        for (int i = 0; i < numberOfCarsPerMinute; i++) {
+            Car car = new AdHocCar();
+            entranceCarQueue.addCar(car);
+        }
+
+        // Remove car from the front of the queue and assign to a parking space.
+        for (int i = 0; i < enterSpeed; i++) {
+            Car car = entranceCarQueue.removeCar();
+            if (car == null) {
+                break;
+            }
+            // Find a space for this car.
+            Location freeLocation = simulatorView.getFirstFreeLocation();
+            if (freeLocation != null) {
+                simulatorView.setCarAt(freeLocation, car);
+                int stayMinutes = (int) (15 + random.nextFloat() * 10 * 60);
+                car.setMinutesLeft(stayMinutes);
+            }
+        }
+
+        // Perform car park tick.
+        simulatorView.tick();
+
+        // Add leaving cars to the exit queue.
+        while (true) {
+            Car car = simulatorView.getFirstLeavingCar();
+            if (car == null) {
+                break;
+            }
+            car.setIsPaying(true);
+            paymentCarQueue.addCar(car);
+        }
+
+        // Let cars pay.
+        for (int i = 0; i < paymentSpeed; i++) {
+            Car car = paymentCarQueue.removeCar();
+            if (car == null) {
+                break;
+            }
+            // TODO Handle payment.
+            simulatorView.removeCarAt(car.getLocation());
+            exitCarQueue.addCar(car);
+        }
+
+        // Let cars leave.
+        for (int i = 0; i < exitSpeed; i++) {
+            Car car = exitCarQueue.removeCar();
+            if (car == null) {
+                break;
+            }
+            // Bye!
+        }
+
+        // Update the car park view.
+
+        simulatorView.updateView();
+
+        // Pause.
+        try {
+            Thread.sleep(tickPause);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void setActionEvent(ActionEvent e) {
+        event = e;
+    }
+
+    public ActionEvent getActionEvent() {
+        return event;
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+        setActionEvent(e);
+        Thread performerThread = new Thread() {
+
+            public void run() {
+                ActionEvent e = getActionEvent();
+                String command = e.getActionCommand();
+
+                switch (command) {
+                    case "one step":
+                        runSteps(1);
+                        break;
+                    case "hundred steps":
+
+                        runSteps(100);
+                }
+            }
+
+        };
+        performerThread.start();
+    }
+
+
+
+    public static void main (String[] args) {
+        new Simulator();
+    }
+
+
+}
